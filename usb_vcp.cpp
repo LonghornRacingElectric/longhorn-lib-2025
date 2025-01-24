e580652aa2fbd2242b11392d861b071d944c15cc
Dhairya Gupta <dhairyag23@gmail.com>
2025-01-23 20:47:57 -0600

    cleaner

--- usb_vcp.cpp
+++ usb_vcp.cpp
@@ -8,13 +8,13 @@
 
 void println(char *buffer) {
     size_t len = strlen(buffer);
+    char buf[len + 3];
+    buf[len + 1] = '\r';
+    buf[len + 2] = '\n';
+    strcpy(buf, buffer);
 #ifdef STM32L4
-    CDC_Transmit_FS((uint8_t *) buffer, len);
-    CDC_Transmit_FS((uint8_t *) "\n", 1);
-    CDC_Transmit_FS((uint8_t *) "\r", 1);
+    CDC_Transmit_FS((uint8_t *) buf, len + 3);
 #elif defined(STM32H7)
-    CDC_Transmit_HS((uint8_t *) buffer, len);
-    CDC_Transmit_HS((uint8_t *) "\n", 1);
-    CDC_Transmit_HS((uint8_t *) "\r", 1);
+    CDC_Transmit_HS((uint8_t *) buf, len + 3);
 #endif
 }
