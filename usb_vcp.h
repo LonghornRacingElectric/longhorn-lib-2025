4da03c4638c258e3611919f5c8a18ba4351ce02d
Dhairya Gupta <dhairyag23@gmail.com>
2025-03-20 23:32:41 -0500

    add vusb_printf

--- usb_vcp.h
+++ usb_vcp.h
@@ -2,22 +2,22 @@
 // Created by Dhairya Gupta on 1/19/25.
 //
 #include <stdint.h>
+
 #include "main.h"
 #ifndef VCU_FIRMWARE_2025_USB_VCP_H
 #define VCU_FIRMWARE_2025_USB_VCP_H
 #ifdef USB_VCP
 
-
 void usb_init();
 
 void println(char *buffer);
 void usb_printf(const char *format, ...);
-void receiveData(uint8_t* data, uint32_t len);
+void vusb_printf(const char *format, va_list args);
+void receiveData(uint8_t *data, uint32_t len);
 void receive_periodic();
 extern volatile uint8_t receivedNotRead;
-
 
 #define DFU_COMMAND "update"
 
 #endif
-#endif //VCU_FIRMWARE_2025_USB_VCP_H
+#endif  // VCU_FIRMWARE_2025_USB_VCP_H
