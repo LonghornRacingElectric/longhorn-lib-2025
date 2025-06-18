55d2c6f49dfb758404ba3812c88995033904f604
matt-mekha <matthew.mekha@gmail.com>
2025-06-17 23:03:43 -0400

    timer improv

--- timer.c
+++ timer.c
@@ -6,9 +6,20 @@
 
 #include "tim.h"
 
+
+static uint64_t lastTickRecorded = 0;
+static uint64_t reload;
+static uint64_t clockFreq;
+
+// deprecated
 static uint32_t lib_timer_prevcycle = 0;
 
-void lib_timer_init() { lib_timer_prevcycle = HAL_GetTick(); }
+void lib_timer_init()
+{
+    lib_timer_prevcycle = HAL_GetTick();
+    clockFreq = HAL_RCC_GetHCLKFreq();
+    reload = clockFreq / 1000;
+}
 
 uint32_t lib_timer_delta_ms() {
     uint32_t cur = HAL_GetTick();
@@ -23,4 +34,23 @@ uint32_t lib_timer_delta_ms() {
     return timeElapsed;
 }
 
-uint32_t lib_timer_elapsed_ms() { return HAL_GetTick(); }
+uint32_t lib_timer_elapsed_ms()
+{
+    return HAL_GetTick();
+}
+
+
+float lib_timer_deltaTime() {
+    uint64_t tick = ((uint64_t)(HAL_GetTick()) * reload) + (reload - SysTick->VAL);
+    float deltaTime = ((float)(tick - lastTickRecorded)) / ((float)clockFreq);
+    if(tick < lastTickRecorded) {
+        return 0;
+    }
+    lastTickRecorded = tick;
+    return deltaTime;
+}
+
+float lib_timer_currentTime() {
+    uint64_t tick = ((uint64_t)(HAL_GetTick()) * reload) + (reload - SysTick->VAL);
+    return ((float)tick) / ((float)clockFreq);
+}
--- timer.h
+++ timer.h
@@ -8,8 +8,12 @@
 
 void lib_timer_init();
 
+// deprecated
 uint32_t lib_timer_delta_ms();
 uint32_t lib_timer_elapsed_ms();
+
+float lib_timer_deltaTime();
+float lib_timer_currentTime();
 
 
 #endif //VCU_FIRMWARE_2025_TIMER_H
